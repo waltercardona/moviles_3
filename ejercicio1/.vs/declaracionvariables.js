688778f0895a75769e4915f1b8df1bd13d5b5283
walter cardona <cardonacardonawalter31@gmail.com>
2020-03-28 13:53:05 -0500

    declaracion de variables

---
+++ ejercicio1/.vs/declaracionvariables.js
@@ -0,0 +1,13 @@
+/*let nombre = "walter";
+let nombre = "samuel";
+console.log(nombre);*/
+
+ let costo = 5;
+ if(costo > 3){
+     let comida = "galleta";
+     if(costo>4){
+         let paseo ="jardin";
+     }
+
+ }
+console.log("la comida: " + comida);
