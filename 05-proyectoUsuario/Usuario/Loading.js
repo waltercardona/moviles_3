1cbfab99937d627f9f6b85b2ad3b16ccaf3c33b3
walter cardona <cardonacardonawalter31@gmail.com>
2020-03-30 20:51:30 -0500

    proyecto de react clase 30/03/2020

---
+++ 05-proyectoUsuario/Usuario/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+export default function Loading() {
+
+    return(
+        <View>
+            <Text>Loading...</Text>
+        </View>
+
+
+    )
+}
---
+++ ProyectoReactNativeNavigation/reactnavigation/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator()
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
+export default App;
